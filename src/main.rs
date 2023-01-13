82dff4fdb7ef551c7df118035a24bda3b60649f3
gypaete02 <elias.albrecher06@gmail.com>
2023-01-14 00:57:38 +0100

    Added a small help page

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,24 @@ mod bspc;
 mod desktops;
 
 fn main() {
+
+    if std::env::args().len() > 1 {
+        println!("{}", HELP);
+        return;
+    }
+
     input::start_listening().unwrap();
 }
+
+const HELP: &'static str = 
+"
+desktop-switcher
+
+desktop-switcher is program for switching between bspwm desktops conveniently with alt + tab.
+
+    Switch between recently used desktops with alt + tab
+    Switch to specific desktop with super + 0..9
+    Switch to next/previous desktop with super + [ / super + ]
+
+    source code at https://github.com/gypaete02/desktop-switcher
+";
